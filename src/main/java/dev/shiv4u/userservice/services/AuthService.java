3a9da53323e06ef5ea0c131b1d37f32eecfd9b8e
Shivnarayan4u <shivnarayanssp_cse16@its.edu.in>
2024-09-06 19:28:05 +0530

    Add BycrptPasswordEncoder

--- src/main/java/dev/shiv4u/userservice/services/AuthService.java
+++ src/main/java/dev/shiv4u/userservice/services/AuthService.java
@@ -1,11 +1,18 @@
 package dev.shiv4u.userservice.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.shiv4u.userservice.dtos.UserDto;
 import dev.shiv4u.userservice.models.Session;
 import dev.shiv4u.userservice.models.SessionStatus;
 import dev.shiv4u.userservice.models.User;
 import dev.shiv4u.userservice.repositories.SessionRepository;
 import dev.shiv4u.userservice.repositories.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -16,7 +23,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 import org.springframework.util.MultiValueMapAdapter;
 
+import javax.crypto.SecretKey;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -24,14 +35,16 @@ public class AuthService {
     private UserRepository userRepository;
     private final SessionRepository sessionRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private SecretKey secretKey;
 
-@Autowired
+    @Autowired
     public AuthService(UserRepository userRepository,
-            SessionRepository sessionRepository,
+                       SessionRepository sessionRepository,
                        BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.sessionRepository = sessionRepository;
         this.userRepository=userRepository;
+        this.sessionRepository = sessionRepository;
         this.bCryptPasswordEncoder=bCryptPasswordEncoder;
+        secretKey = Jwts.SIG.HS256.key().build();
     }
 
     public ResponseEntity<UserDto> login(String email,String password) {
@@ -40,10 +53,19 @@ public class AuthService {
            return null;
        }
        User user=userOptional.get();
-       /*if(!bCryptPasswordEncoder.matches(password,user.getPassword())){
+       if(!bCryptPasswordEncoder.matches(password,user.getPassword())){
            throw new RuntimeException("Password/username does not match");
-       }*/
-       String token = RandomStringUtils.randomAlphanumeric(30);
+       }
+       //String token = RandomStringUtils.randomAlphanumeric(30);
+        Map<String,Object> jwtData=new HashMap<>();
+        jwtData.put("email",email);
+        jwtData.put("createdAt",new Date());
+        jwtData.put("expiryAt",new Date(LocalDate.now().plusDays(3).toEpochDay()));
+        String token = Jwts
+                .builder()
+                .claims(jwtData)
+                .signWith(secretKey)
+                .compact();
         Session session=new Session();
         session.setSessionStatus(SessionStatus.ACTIVE);
         session.setToken(token);
@@ -52,7 +74,7 @@ public class AuthService {
         UserDto userDto=UserDto.from(user);
         MultiValueMap<String, String> headers = new MultiValueMapAdapter<>(new HashMap<>());
         headers.add(HttpHeaders.SET_COOKIE, "auth-token:" + token);
-        return new ResponseEntity<>(userDto,headers, HttpStatus.OK);
+        return new ResponseEntity<UserDto>(userDto,headers, HttpStatus.OK);
     }
 
     public void logout(String token,Long userId) {
@@ -68,7 +90,7 @@ public class AuthService {
     public UserDto signUp(String email, String password) {
         User user=new User();
         user.setEmail(email);
-        user.setPassword(password);
+        user.setPassword(bCryptPasswordEncoder.encode(password));
         User savedUser=userRepository.save(user);
         return UserDto.from(savedUser);
     }
@@ -81,6 +103,12 @@ public class AuthService {
         if(!session.getSessionStatus().equals(SessionStatus.ACTIVE)){
             return SessionStatus.ENDED;
         }
+        Jws<Claims> claimsJws=Jwts
+                .parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token);
+        String email=(String) claimsJws.getPayload().get("email");
         return SessionStatus.ACTIVE;
     }
 }
